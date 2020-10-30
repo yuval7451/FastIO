806d0b03001df946567746847185df246c174df7
yuval kaneti <yuval.kaneti01@gmail.com>
2020-10-31 01:57:13 +0200

    Added CopyFiles & a better Copydir

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # Author: Yuval Kaneti
 
-__version__ = "1.0.0"
-__all__ = ["walk", "CopyFile", "CopyDir"]
+__version__ = "1.0.1"
+__all__ = ["walk", "CopyFile", "CopyFiles", "CopyDir", "CopyDirExecutor"]
 __doc__ = "An Asynchronouse shutil & os Like Module for Fast File IO"
--- tests/performance_test.py
+++ tests/performance_test.py
@@ -6,7 +6,7 @@ from FastIO import CopyDir, Logger
 
 async def main():
     start = time.time()
-    await CopyDir(r"D:\FastIO\1GB\src", r"D:\FastIO\1GB\dst")
+    await CopyDir(r"C:\Python27", r"D:\FastIO\Python27")
     end = time.time() - start
     Logger.info("it took {} Seconds".format(end))
 asyncio.run(main())
