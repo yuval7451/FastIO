e4078f4bc4e46ece7ebadbb299511a4b54c72314
yuval kaneti <yuval.kaneti01@gmail.com>
2020-10-31 19:14:34 +0200

    Removed tests

--- tests/copyfile_test.py
+++
@@ -1,9 +0,0 @@
-# Author: Yuval Kaneti
-# Backup Kernel32.dll
-
-import asyncio
-from FastIO import CopyFile
-async def main():
-   await CopyFile(r"C:\Windows\System32\kernel32.dll", r"C:\kernel32.dll.backup")    
-
-asyncio.run(main())
